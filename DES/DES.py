ce0231d1e86c5bd93fbf7792bae26db02a07a24a
Valera <NsT.valerka@ukr.net>
2018-04-06 13:06:38 +0300

    refactoring + decrypt

--- DES/DES.py
+++ DES/DES.py
@@ -1,7 +1,5 @@
 from Cryptodome.Cipher import DES
 
-key = b'aaaaaaaa' # Ключевое слово
-
 
 # Считываемтекст с файла
 def get_text():
@@ -10,24 +8,39 @@ def get_text():
 
 
 # Добавляем дополнительные байты, если их число не кратно 8
-def pad(text):
-    while len(text) % 8 != 0:
-        text += b'\x00'
-    return text
-
-des = DES.new(key, DES.MODE_ECB)
-text = get_text()
-padded_text = pad(text)
+def pad(byte_array):
+    while len(byte_array) % 8 != 0:
+        byte_array += b'\x00'
+    return byte_array
 
 
-encrypted_text = des.encrypt(padded_text)
+def des_encrypt(des_obj, text, file):
+    padded_text = pad(text)
 
-with open("text_encrypt!.txt", 'wb') as f1:
-    f1.write(encrypted_text)
+    encrypted_text = des_obj.encrypt(padded_text)
+    with open(file, 'wb') as f1:
+        f1.write(encrypted_text)
 
-s = bytes
-with open("text_encrypt!.txt", 'rb') as f1:
-    s = f1.read()
 
-dec_text = des.decrypt(s)
-print("Dec text:", dec_text)
+def des_decrypt(des_obj, file_encrypt, file_decrypt):
+    with open(file_encrypt, 'rb') as f1:
+        s = f1.read()
+
+    dec_text = des_obj.decrypt(s)
+
+    with open(file_decrypt, 'wb') as f1:
+        f1.write(dec_text)
+
+
+if __name__ == '__main__':
+    # Задаем мод шифрования
+    key = b'asterisk'  # Ключевое слово
+    des = DES.new(key, DES.MODE_ECB)
+
+    text = get_text()
+    file_name_encrypt = "text_encrypt_DES.txt"
+    file_name_decrypt = "text_decrypt_DES.txt"
+
+    des_encrypt(des, text, file_name_encrypt)
+    des_decrypt(des, file_name_encrypt, file_name_decrypt)
+
