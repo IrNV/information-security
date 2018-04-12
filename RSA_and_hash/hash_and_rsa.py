e8f44d11e0f72786e511024c29b5334d6776a53c
Valera <NsT.valerka@ukr.net>
2018-04-12 17:50:36 +0300

    add symmetric decryption, comments and new names

--- RSA_and_hash/hash_and_rsa.py
+++ RSA_and_hash/hash_and_rsa.py
@@ -3,52 +3,80 @@ from hashlib import sha1
 import random
 
 
-def symmetric():
+def create_symmetric_key():
+    """
+    Создаем случайный ключ из случайно последовательности чисел
+    """
     random_numbers = [str(i) for i in range(40)]
     random.shuffle(random_numbers)
 
     return ",".join(random_numbers)
 
 
-def symmetric_encryption(key_symetric, message):
+def symmetric_encryption(key_symmetric, message):
+    """
+    Шифруем простой перестановкой
+    """
     encrypted_message = list(message)
-    key = key_symetric.split(",")
+    key = key_symmetric.split(",")
 
-    print("me", message)
-    print("key", key)
     for i, v in enumerate(message):
-        new_positon = key[i]
-        encrypted_message[int(new_positon)] = chr(v)
-
-    print(encrypted_message)
+        new_position = key[i]
+        encrypted_message[int(new_position)] = chr(v)
 
     return "".join(encrypted_message)
 
 
+def symmetric_decryption(key_symmetric, message):
+    """
+    Дешифруем простую перестановку
+    """
+    decrypted_message = list(message)
+    key = key_symmetric.split(",")
+
+    for i, v in enumerate(key):
+        decrypted_message[i] = chr(message[int(v)])
+
+    return "".join(decrypted_message)
+
+
 def get_bob_message(alice_pub):
+    """
+    Формируем сообщение от Боба, зашифрованный простой перестановкой хеш этого сообщения и
+    зашифрованный ключ простой перестановки шифром rsa.
+    """
     message = 'hello Alice!'.encode('utf8')
     hash_message = sha1(message)
-    print(hash_message.hexdigest())
 
-    key_symetric = symmetric()
-    print(key_symetric)
+    key_symmetric = create_symmetric_key()  # Создаем ключ для простой перестановки
 
-    encrypted_hash = symmetric_encryption(key_symetric, hash_message.hexdigest().encode("utf8"))
-    print("Encrypted_hash:", encrypted_hash)
+    # Шифруем хеш
+    encrypted_hash = symmetric_encryption(key_symmetric, hash_message.hexdigest().encode("utf8"))
 
-    encrypted_key_symetric = rsa.encrypt(key_symetric.encode('utf8'), alice_pub)
+    # Шифруем ключ симметрического шифрования (шифр простой перестановки)
+    encrypted_symmetric_key = rsa.encrypt(key_symmetric.encode('utf8'), alice_pub)
 
-    return message, encrypted_hash, encrypted_key_symetric
+    return message, encrypted_hash, encrypted_symmetric_key
 
 
 def alice_function():
     (alice_pub, alice_priv) = rsa.newkeys(1024)
-    message, encrypted_hash, encrypted_key_symetric = get_bob_message(alice_pub)
-    key_symetric = rsa.decrypt(encrypted_key_symetric, alice_priv)
+    message, encrypted_hash, encrypted_symmetric_key = get_bob_message(alice_pub)
 
-    print(key_symetric)
-    #print("asdasd".encode("utf8"))
-    print(symmetric_encryption(key_symetric.decode('utf8'), encrypted_hash.encode("utf8")))
+    print("Alice receive from Bob message:", message)
+    print("Alice receive from Bob encrypted hash:", encrypted_hash)
+    print("Alice receive from Bob encrypted symmetric key:", encrypted_symmetric_key)
+
+    symmetric_key = rsa.decrypt(encrypted_symmetric_key, alice_priv)
+    print("Alice has decrypted Bob's key by her private key:", symmetric_key)
+    decrypted_hash = symmetric_decryption(symmetric_key.decode('utf8'), encrypted_hash.encode("utf8"))
+    print("Alice has decrypted hash by symmetric key:", decrypted_hash)
+
+    if decrypted_hash == sha1(message).hexdigest():
+        print("\n")
+        print("decrypted hash:", decrypted_hash)
+        print("sh1(message from Bob):", sha1(message).hexdigest())
+        print("Alice receive right message!")
 
 
 def main():
