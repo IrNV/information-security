88f0d225c12941ab36fb4e547d9a1e98b0dfc591
Valera <NsT.valerka@ukr.net>
2018-04-13 14:30:15 +0300

    find inf from picture

--- Steganography/steganography.py
+++ Steganography/steganography.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 
-def create_bin_code(symbol):
+def create_symbol_bin_code(symbol):
     len_bits = bin(ord(symbol))[2::]
     extra_zero = "0b"
     for i in range(len(len_bits), 8):
@@ -9,7 +9,17 @@ def create_bin_code(symbol):
 
     return extra_zero + len_bits
 
-message = "Hello!"
+
+def create_number_bin_code(number):
+    len_bits = bin(number)[2::]
+
+    extra_zero = "0b"
+    for i in range(len(len_bits), 8):
+        extra_zero += "0"
+
+    return extra_zero + len_bits
+
+message = "Hello!   My friend!"
 im = Image.new("RGB", (100, 100))
 pixelsNew = im.load()
 
@@ -18,7 +28,7 @@ img = Image.open("testik.bmp")
 iterator = 0
 rgb_im = img.convert('RGB')
 for symbol in message:
-    bin_code = create_bin_code(symbol)
+    bin_code = create_symbol_bin_code(symbol)
     print("Symbol:", symbol, " -->", bin_code)
     for i in range(0, 8, 2):
         pixel_r, p2, p3 = rgb_im.getpixel((iterator % 100, iterator // 100))
@@ -35,10 +45,17 @@ f = open("encrypted.bmp", "wb")
 im.save(f, "BMP")
 rgb_im = im.convert('RGB')
 
-for i in range(12):
-    pixel_r, p2, p3 = rgb_im.getpixel((i, 0))
-    print("x, y:", (i, 0), "Value:", pixel_r, p2, p3)
 
+img = Image.open("encrypted.bmp")
+rgb_im = img.convert('RGB')
+symbol = "0b"
+text = ""
+for i in range(10000):
+    pixel_r, p2, p3 = rgb_im.getpixel((i % 100, i // 100))
+    last_bits = bin(pixel_r)[-2]
+    symbol += create_number_bin_code(pixel_r)[-2::]
+    if len(symbol) == 10:
+        text += chr(int(symbol, 2))
+        symbol = "0b"
 
-
-
+print(text)
